f0b4189a3b2fc04dbc265132adcaf2a2d26a21cf
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-10 22:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 function calculateMinCost() {
+	let CONTAINER = document.getElementById('result');
+	if (CONTAINER !== '') {
+		return;
+	}
   //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
-  
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
-  }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
-}
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
+	let str = document.getElementById('rope-lengths').value;
+    let arr = str.split(',');
+	let numArr = arr.map((ele) => {
+		return parseInt(ele);
+	})
+	let cost = 0;
+	while(numArr.length > 1){
+		numArr.sort((a,b) => a-b);
+		cost += (numArr[0]+numArr[1]);
+		numArr.shift();
+		numArr.shift();
+		numArr.push(cost);
+	} 
+	
+	CONTAINER.appendChild(cost);
+	return cost;
 }  
